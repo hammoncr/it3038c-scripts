18ef4ad46ed83d1321e31ee60013be5c3f285389
hammoncr <hammoncr@mail.uc.edu>
2020-09-20 18:34:46 -0400

    add project 1 script

---
+++ powershell/runningservices.ps1
@@ -0,0 +1,3 @@
+﻿Write-Host "This script will give you the status of all the services on your machine"
+Write-Host "The status of your running services is:"
+Get-CimInstance -ClassName Win32_Service | Format-Table -Property Status,Name,DisplayName -AutoSize -Wrap
